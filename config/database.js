31814dddccda4d5677a362be61fd620b3834617b
HARU <chu_ba@naver.com>
2023-05-27 11:51:06 +0900

    node practice _ 4

---
+++ config/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const databaseAddress = process.env.MONGO_URL
+
+        await mongoose.connect(databaseAddress)
+
+        console.log("connected database")
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export default connectDB()
